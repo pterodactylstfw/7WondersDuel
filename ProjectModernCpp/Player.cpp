96d0c005fea184d66f62e0e3e57c470f8cb86ecf
DenisaCozma <d22889526@gmail.com>
2025-11-08 22:31:33 +0200

    Added new functions to Player.cpp

--- ProjectModernCpp/Player.cpp
+++ ProjectModernCpp/Player.cpp
@@ -19,6 +19,51 @@ void Player::addProgressToken(std::unique_ptr<ProgressToken>&& token)
 	progressTokens.push_back(std::move(token));
 }
 
+void Player::addResource(ResourceType type, int qty)
+{
+	resourceProduction.addFixedResource(type, qty);
+}
 
+void Player::addResourceChoice(std::vector<ResourceType>& choices)
+{
+	resourceProduction.addChoice(choices);
+}
 
+void Player::addCoins(int amount)
+{
+	coins += amount;
+}
 
+bool Player::removeCoins(int amount)
+{
+	if (coins < amount)
+		return false;
+	coins -= amount;
+	return true;
+}
+
+void Player::addMilitaryShields(int shields)
+{
+	militaryShields += shields;
+}
+
+void Player::addScientificSymbol(ScientificSymbol symbol)
+{
+	scientificSymbols[symbol] += 1;
+}
+
+void Player::addVictoryPoints(int points)
+{
+	victoryPoints += points;
+}
+
+bool Player::canAfford(const Cost& cost, const Player& opponent) const
+{
+	const int totalCoinCost = calculateResourceCost(cost, opponent);
+	return coins >= totalCoinCost;
+}
+
+int Player::calculateResourceCost(const Cost& cost, const Player& opponent) const
+{
+
+}

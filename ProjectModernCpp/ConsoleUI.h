5da8702be459bea738f1540d367eaae73982b8d6
Raul-Nicolae Constantin <constantin.raul10@gmail.com>
2025-11-27 09:06:22 +0200

    Add ConsoleUI class and Utils for user input handling

---
+++ ProjectModernCpp/ConsoleUI.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "GameController.h"
+class ConsoleUI
+{
+private:
+	GameController m_game;
+
+	void displayHeader() const;
+	void displayGameState() const;
+
+	void displayPlayer(const Player& player) const;
+
+	void displayWonderBoard() const;
+
+	void displayPyramid() const;
+
+	int showMainMenu();
+	int showActionMenu();
+	void showVictoryScreen(const std::string& winnerName);
+
+public:
+
+	void run();
+
+};
+

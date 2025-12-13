5358678b62aa482fa071a9e225aa2f3a8607252f
Cristian <cristian.ciulica@student.unitbv.ro>
2025-12-13 10:07:35 +0200

    Add AIController class

---
+++ src/controller/AIController.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "GameState.h"
+#include "Constants.h"
+#include <vector>
+#include <optional>
+enum class AIDifficulty {
+	EASY,
+	HARD
+};
+struct AIMove {
+	int cardIndex;
+	PlayerAction action;
+	int wonderIndex = -1;
+};
+
+class AIController {
+private:
+	AIDifficulty m_difficulty;
+
+	AIMove getRandomMove(const GameState& state);
+	AIMove getGreedyMove(const GameState& state);
+	double evaluateCardValue(const Card& card, const Player& player, const Player& opponent, const GameState& state);
+public:
+	AIController(AIDifficulty difficulty);
+	AIMove decideMove(const GameState& state);
+
+};

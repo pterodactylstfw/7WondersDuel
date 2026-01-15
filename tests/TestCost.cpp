e762a4b2dcc75e846d04554cad42e8427720b076
Cristian <cristian.ciulica@student.unitbv.ro>
2026-01-15 18:44:27 +0200

    Add tests for Cost affordability and trading logic

--- tests/TestCost.cpp
+++ tests/TestCost.cpp
@@ -22,3 +22,27 @@ TEST(CostTest, Initaialization_And_Getters) {
 	EXPECT_EQ(complexCost.getResourceCost(ResourceType::STONE), 2);
 	EXPECT_EQ(complexCost.getResourceCost(ResourceType::GLASS), 0);
 }
+TEST(CostTest, CanAfford_Logic) {
+	Cost cost;
+	cost.withCoinCost(3).withResourceCost(ResourceType::CLAY, 1);
+
+	std::map<ResourceType, int> myResources;
+	myResources[ResourceType::CLAY] = 1;
+	int myCoins = 5;
+	EXPECT_TRUE(cost.canAfford(myCoins, myResources));
+	EXPECT_FALSE(cost.canAfford(2, myResources));
+
+	myResources[ResourceType::CLAY] = 0;
+	EXPECT_FALSE(cost.canAfford(10, myResources));
+}
+TEST(CostTest, TradingCost_Calculation) {
+	Cost cost;
+	cost.withResourceCost(ResourceType::WOOD, 2);
+	std::map<ResourceType, int> myResources;
+	std::map<ResourceType, int> opponentProduction;
+
+	opponentProduction[ResourceType::WOOD] = 0;
+	EXPECT_EQ(cost.calculateTradingCost(myResources, opponentProduction), 4);
+	opponentProduction[ResourceType::WOOD] = 1;
+	EXPECT_EQ(cost.calculateTradingCost(myResources, opponentProduction), 6);
+}
